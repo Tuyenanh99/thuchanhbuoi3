c4a4f34f79096be75c942cdfaacc72342d2504ee
Tuyenanh99 <tuyentruong57@gmail.com>
2020-10-14 15:49:46 +0700

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TH03'`, () => {
+  it(`should have as title 'BaiTH3b2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TH03');
+    expect(app.title).toEqual('BaiTH3b2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('TH03 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('BaiTH3b2 app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TH03';
+  title = 'BaiTH3b2';
 }
